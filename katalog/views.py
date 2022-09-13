512eeea966482188225325b2e53b525a265b6c62
maritzarahayu <maritzarahayui@gmail.com>
2022-09-13 11:36:55 +0700

    add katalog

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_barang_katalog = CatalogItem.objects.all()
+    context = {
+    'list_katalog': data_barang_katalog,
+    'nama': 'Caca',
+    'student_id': '2106751474'
+    }
+    return render(request, "katalog.html", context)

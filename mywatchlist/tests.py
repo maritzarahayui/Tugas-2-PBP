1569445842e62e9530630933f9d23e1ed78abf16
maritzarahayu <maritzarahayui@gmail.com>
2022-09-21 22:26:36 +0700

    fix tests.py

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -4,17 +4,17 @@ from django.test import Client
 # Create your tests here.
 class MyWatchListTest(TestCase):
     def test_mywatchlist(self):
-        response = Client().get('/mywatchlist')
+        response = Client().get('/mywatchlist/')
         self.assertEqual(response.status_code,200)
 
-    def test_mywatchlist(self):
+    def test_mywatchlist_for_xml(self):
         response = Client().get('/mywatchlist/xml/')
         self.assertEqual(response.status_code,200)
 
-    def test_mywatchlist(self):
+    def test_mywatchlist_for_json(self):
         response = Client().get('/mywatchlist/json/')
         self.assertEqual(response.status_code,200)
 
-    def test_mywatchlist(self):
+    def test_mywatchlist_for_html(self):
         response = Client().get('/mywatchlist/html/')
         self.assertEqual(response.status_code,200)
